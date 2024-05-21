41ec64362731147a6f7398d5b9f4d80718b03fe1
Luiz Guilherme F. W <104701221+eziul@users.noreply.github.com>
2024-05-20 22:52:03 -0300

    Primeira issue

---
+++ landing-page-vet/scripts.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+  $('.slick-carousel').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
+
+function toggleInfo(element) {
+  // Esconder todas as outras informações
+  $('.service-info').not($(element).next()).slideUp();
+
+  // Toggle a informação clicada
+  $(element).next('.service-info').slideToggle();
+}
