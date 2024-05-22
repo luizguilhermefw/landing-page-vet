403d79348f8c100c48aecc58c2bd10aeedc07acb
Luiz Guilherme F. W <104701221+eziul@users.noreply.github.com>
2024-05-21 22:27:06 -0300

    novas funções e ajustes no layout

--- landing-page-vet/scripts.js
+++ landing-page-vet/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     arrows: false,
     infinite: true,
     speed: 300,
-    slidesToShow: 4,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
@@ -12,7 +12,7 @@ $(document).ready(function(){
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 4,
+          slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
           dots: true
